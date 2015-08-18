84a6ca9aba1cc1c3343f4ec968d73b48abce0afe
Akbar Mirza <akbarbmirza@gmail.com>
2015-08-18 10:26:12 -0500

    Move Demo Styles in demo html

--- js/img-slider.js
+++ js/img-slider.js
@@ -1,5 +1,15 @@
-$(document).ready(
-  $('.img-slider').click(function () {
-    $(this).children('.caption').toggleClass( 'collapse' );
+var imageSlider = {};
+
+imageSlider.init = function ( options ) {
+  var slider = $(options.slider);
+  var caption = options.caption;
+  var projName = options.projName;
+
+  slider.addClass('ics-slider');
+  $(caption).addClass('ics-caption');
+  $(projName).addClass('ics-proj-name');
+
+  slider.click(function () {
+    $(this).children(caption).toggleClass( 'ics-collapse' );
   })
-)
+};
