fb3096efd539bda83fba1d77c2d1ba9bf2f201e4
Akbar Mirza <akbarbmirza@gmail.com>
2015-08-19 16:50:12 -0500

    Update img-slider to support collapse on start

--- js/img-slider.js
+++ js/img-slider.js
@@ -5,11 +5,14 @@ imageSlider.init = function ( options ) {
   var caption = options.caption;
   var title = options.title;
   var desc = options.desc;
+  var collapse = options.collapse;
 
   slider.addClass( 'ics-slider' );
   $( caption ).addClass( 'ics-caption' );
   $( title ).addClass( 'ics-title' );
   $( desc ).addClass( 'ics-desc' );
+  $( collapse ).addClass( 'ics-collapse' );
+
 
   slider.click(function () {
     $( this ).children( caption ).toggleClass( 'ics-collapse' );
