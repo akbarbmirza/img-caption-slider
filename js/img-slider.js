7d7f20263ece39ad42ae90d38866e9875b08e1e7
Akbar Mirza <akbarbmirza@gmail.com>
2015-08-19 14:25:10 -0500

    Add desc handler

--- js/img-slider.js
+++ js/img-slider.js
@@ -4,10 +4,12 @@ imageSlider.init = function ( options ) {
   var slider = $( options.slider );
   var caption = options.caption;
   var title = options.title;
+  var desc = options.desc;
 
   slider.addClass( 'ics-slider' );
   $( caption ).addClass( 'ics-caption' );
   $( title ).addClass( 'ics-title' );
+  $( desc ).addClass( 'ics-desc' );
 
   slider.click(function () {
     $( this ).children( caption ).toggleClass( 'ics-collapse' );
